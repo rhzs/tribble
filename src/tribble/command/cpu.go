e30067301e7762e5737da5959600ab135f1e7c64
incubus8 <rheza.satria.ta@gmail.com>
2015-08-13 23:55:21 +0700

    Feature: Add list of CPU usage per application/process

---
+++ src/tribble/command/cpu.go
@@ -0,0 +1,68 @@
+package command
+
+import (
+	"bytes"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type Process struct {
+	pid int
+	cpu float64
+}
+
+type CpuCommand struct {
+	Meta
+}
+
+func (c *CpuCommand) Run(args []string) int {
+	cmd := exec.Command("ps", "aux")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	processes := make([]*Process, 0)
+	for {
+		line, err := out.ReadString('\n')
+		if err!=nil {
+			break;
+		}
+		tokens := strings.Split(line, " ")
+		ft := make([]string, 0)
+		for _, t := range(tokens) {
+			if t!="" && t!="\t" {
+				ft = append(ft, t)
+			}
+		}
+		log.Println(len(ft), ft)
+		pid, err := strconv.Atoi(ft[1])
+		if err!=nil {
+			continue
+		}
+		cpu, err := strconv.ParseFloat(ft[2], 64)
+		if err!=nil {
+			log.Fatal(err)
+		}
+		processes = append(processes, &Process{pid, cpu})
+	}
+	for _, p := range(processes) {
+		log.Println("Process ", p.pid, " takes ", p.cpu, " % of the CPU")
+	}
+	return 0
+}
+
+func (c *CpuCommand) Synopsis() string {
+	return "Get memory free status"
+}
+
+func (c *CpuCommand) Help() string {
+	helpText := `
+Get	cpu usage
+`
+	return strings.TrimSpace(helpText)
+}
+
--- src/tribble/commands.go
+++ src/tribble/commands.go
@@ -12,8 +12,12 @@ func Commands(meta *command.Meta) map[string]cli.CommandFactory {
 				Meta: *meta,
 			}, nil
 		},
-		
-        "version": func() (cli.Command, error) {
+		"cpu": func() (cli.Command, error) {
+			return &command.CpuCommand{
+				Meta: *meta,
+			}, nil
+		},
+		"version": func() (cli.Command, error) {
 			return &command.VersionCommand{
 				Meta:     *meta,
 				Version:  Version,
